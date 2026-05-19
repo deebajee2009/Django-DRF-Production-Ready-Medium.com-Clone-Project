1784f0b657d870346daa1dd63b31e382f9b5b2b3
deebajee2009 <deebaje2009@gmail.com>
2026-05-20 00:31:22 +0330

    feat: configure celery, redis and flower

---
+++ authors_api/celery.py
@@ -0,0 +1,16 @@
+"""
+Celery Configurations for project
+"""
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# TODO: change this in production
+os.environ.setdefault("Django_SETTINGS_MODULE", "authors_api.settings.local")
+
+app = Celery("authors_api")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -12,3 +12,10 @@ DEBUG = True
 
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@example.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors api"
