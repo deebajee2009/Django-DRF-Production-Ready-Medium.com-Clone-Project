f6f7217600dbca3a5ed253b776026fa168a30044
deebajee2009 <deebaje2009@gmail.com>
2026-06-23 04:13:17 +0330

    test: user models, forms, serializers & views

---
+++ core_apps/users/tests/test_forms.py
@@ -0,0 +1,37 @@
+"""
+users app test forms.
+"""
+import pytest
+from core_apps.users.forms import UserCreationForm
+from core_apps.users.tests.factories import UserFactory
+
+
+@pytest.mark.django_db
+def test_user_creation_form_valid_data():
+    data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "john.doe@example.com",
+        "password1": "secure_pass",
+        "password2": "secure_pass",
+    }
+
+    form = UserCreationForm(data)
+    assert form.is_valid()
+
+
+@pytest.mark.django_db
+def test_user_creation_form_invalid_data():
+    user = UserFactory()
+
+    data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": user.email,
+        "password1": "secure_pass_123",
+        "password2": "secure_pass_123",
+    }
+
+    form = UserCreationForm(data)
+    assert not form.is_valid()
+    assert  "email" in form.errors
